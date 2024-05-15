708ab2b830dc8143eb8e887ae5479c586b4421c2
MedAmineMahid <med.amine.mahid@gmail.com>
2024-05-15 09:46:00 +0100

    version8

--- src/main/java/gym/projetj2ee/Security/SecurityConfig.java
+++ src/main/java/gym/projetj2ee/Security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorize) ->
                         authorize
-                                .requestMatchers("/Client/home","/Client/**").permitAll()
+                                .requestMatchers("/Client/home","/Client/**","/photos/**").permitAll()
                                 .anyRequest().authenticated()
                 ).formLogin(
                         form -> form
